9178d01df586a7774d4632503e5567a89bb718ce
Ashlynne Mitchell <ovy@ovyerus.com>
2025-06-07 22:45:11 +1000

    chore: update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Atex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/cometsh/atex"
+
   def project do
     [
       app: :atex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "atex",
+      description: "A set of utilities for working with the AT Protocol in Elixir.",
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -21,6 +28,26 @@ defmodule Atex.MixProject do
     [
       {:typedstruct, "~> 0.5"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        LICENSE: [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatters: ["html"]
     ]
   end
 end
